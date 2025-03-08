b09ca5802b617891fa2067b7c4ba8d25de1ca1dc
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-08 02:37:18 +0200

    Updates

---
+++ JS/Exercise 03/03.js
@@ -0,0 +1,11 @@
+function print(char1, char2){
+    const char1Num = char1.charCodeAt(0);
+    const char2Num = char2.charCodeAt(0);
+    const chars = [];
+    for (let i =  Math.min(char1Num, char2Num); i < Math.max(char1Num, char2Num); i++) {
+        chars.push(String.fromCharCode(i));
+    }
+    console.log(chars.join(' '))
+}
+
+print('C', '#');
