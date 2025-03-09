6c11750e5c8ec6727bc218bcdbfb5920185e2915
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-10 01:41:34 +0200

    Updates

--- JS/Exercise 03/02.js
+++ JS/Exercise 03/02.js
@@ -1,6 +1,7 @@
 function subtract(num1, num2, num3){
-    const result = (num1, num2) => n1 + n2;
-    console.log(result - num3);
-}
+    console.log((sum(num1, num2)) - num3);
 
-subtract(1, 2, 3);
+    function sum(n1, n2){
+        return n1+n2;
+    }
+}
