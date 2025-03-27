ebb6761b4929ddc42e98e7e664302b3e0fb41b85
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 22:17:56 +0200

    Task 03

--- JS/Exercise 05/03. Accordion/accordion.js
+++ JS/Exercise 05/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    //TODO
+    let button = document.querySelector(".button");
+    let text = document.querySelector("#extra");
+
+    if(text.style.display === "none" && button.textContent === 'More'){
+        text.style.display = "block"
+        button.textContent = "Less"
+    }
+    else{
+        text.style.display = "none"
+        button.textContent = "More"
+    }
 }
