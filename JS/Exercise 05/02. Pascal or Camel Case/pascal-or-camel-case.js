fc49f503c5c317970c74a1913ed19466a39709d8
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 21:49:05 +0200

    Task 02

--- JS/Exercise 05/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ JS/Exercise 05/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,27 @@
-function solve() {
-  //TODO
+function solve(text, naming) {
+    let casing = document.querySelector("#naming-convention").value;
+    let input = document.querySelector("#text").value;
+    let resultContainer = document.querySelector("#result");
+
+    const words = input
+        .toLowerCase()
+        .trim()
+        .split(/[\s_-]+/)
+        .filter(word => word.length > 0);
+
+    switch (casing) {
+      case "Camel Case":
+        resultContainer.textContent = words[0] + words.slice(1).map(capitalize).join('');
+        break;
+      case "Pascal Case":
+        resultContainer.textContent = words.map(capitalize).join('');
+        break;
+      default:
+        resultContainer.textContent = 'Error!';
+        break;
+    }
+  
+    function capitalize(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
 }
