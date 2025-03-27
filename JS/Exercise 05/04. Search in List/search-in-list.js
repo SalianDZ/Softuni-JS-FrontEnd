456a5baf9da0203b9f1c0987962eeeaafed808d5
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-28 00:31:25 +0200

    Task 04

--- JS/Exercise 05/04. Search in List/search-in-list.js
+++ JS/Exercise 05/04. Search in List/search-in-list.js
@@ -1,3 +1,24 @@
 function solve() {
    // TODO
+   let towns = document.querySelectorAll("#towns li");
+   let searchString = document.querySelector("#searchText").value.toLowerCase();
+   let resultElement = document.querySelector("#result");
+
+   if (searchString == '') return;
+
+
+   towns.forEach(town => {
+      
+      town.classList.remove('match'); 
+      town.style.fontWeight = "notmal";
+      town.style.textDecoration = "none";
+
+         if(town.textContent.toLowerCase().includes(searchString)){
+            console.log(town.textContent, "Found ");
+            town.classList.add('match'); 
+            town.style.fontWeight = "bold";
+            town.style.textDecoration = "underline";
+         }
+   }); 
+   resultElement.textContent = `${document.querySelectorAll('.match').length} matches found`;
 }
