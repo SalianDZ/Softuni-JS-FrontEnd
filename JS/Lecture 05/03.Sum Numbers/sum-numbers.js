e903ed923a5d3441b8fdc74c00752cd52a045157
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 18:05:54 +0200

    Task 03

--- JS/Lecture 05/03.Sum Numbers/sum-numbers.js
+++ JS/Lecture 05/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,6 @@
 function calc() {
     // TODO
+    let number1 = Number(document.querySelector('#num1').value);
+    let number2 = Number(document.querySelector('#num2').value);
+    document.querySelector('#sum').value = number1 + number2;
 }
