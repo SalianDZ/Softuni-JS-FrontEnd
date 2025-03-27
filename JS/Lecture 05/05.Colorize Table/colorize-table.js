410c642f3b005312f8ede91fc6a5c0078d5f7abb
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 18:20:58 +0200

    Task 05

--- JS/Lecture 05/05.Colorize Table/colorize-table.js
+++ JS/Lecture 05/05.Colorize Table/colorize-table.js
@@ -1,3 +1,7 @@
 function colorize() {
-    //TODO
-}
+    let rows = document.querySelectorAll('table tr:nth-child(even)');
+ 
+    for (let row of rows) {
+         row.style.background = 'teal';
+    }
+ }
