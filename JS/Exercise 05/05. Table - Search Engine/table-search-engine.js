1af6bcdf445e10b46b39438ea92ea52df338e4d4
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-28 01:20:45 +0200

    Task 05

--- JS/Exercise 05/05. Table - Search Engine/table-search-engine.js
+++ JS/Exercise 05/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,19 @@
 function solve() {
-   //TODO
+   let searchString = document.querySelector("#searchField").value.toLowerCase();
+
+   if(searchString == '') return;
+
+   const rows = document.querySelectorAll("table tbody tr");
+   [...rows].map(r => r.classList.remove('select'));
+
+   for (let row of rows) {
+      const cells = row.querySelectorAll('td');
+
+      for (let cell of cells) {
+        if (cell.textContent.toLowerCase().includes(searchString)) {
+         row.classList.add('select');
+         break;
+        }
+      }
+   }
 }
