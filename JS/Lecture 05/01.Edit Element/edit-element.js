579a75d1363be56ed69019f86bf7326b34bc2489
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 17:51:34 +0200

    Task 01

--- JS/Lecture 05/01.Edit Element/edit-element.js
+++ JS/Lecture 05/01.Edit Element/edit-element.js
@@ -1,3 +1,8 @@
-function editElement() {
-    // TODO
+function editElement(inputElement, match, replacer) {
+
+    const element = inputElement.textContent;
+    const matcher = new RegExp(match, "g");
+    const editedContent = element.replace(matcher, replacer);
+
+    inputElement.textContent = editedContent;
 }
