ea0437f21e47271f57bfe69fd8ed5192da6d6124
Salian Dzhihanov <saliandzhihanov@gmail.com>
2025-03-27 21:32:48 +0200

    Exercise 05 Tasks

---
+++ JS/Exercise 05/01. Subtraction/subtraction.js
@@ -0,0 +1,6 @@
+function subtract() {
+    //TODO
+    let number1 = Number(document.querySelector("#firstNumber").value);
+    let number2 = Number(document.querySelector("#secondNumber").value);
+    document.querySelector("#result").textContent = number1 - number2;
+}
